1faefc112cc58b738c14b0ff00c9e0928d4c707c
Roman Kuba <mail@codebryo.com>
2015-03-18 10:03:46 +0100

    Added build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var autoprefixer= require('gulp-autoprefixer');
+var rename      = require("gulp-rename");
+
+var config      = require('../config');
+
+gulp.task('build', function() {
+
+  return gulp.src(config.sass.src)
+    .pipe(sass())
+    .on('error', function (err) {
+      console.log(err);
+    })
+    .pipe(autoprefixer(
+      config.sass.autoprefixer
+    ))
+    .pipe(rename('rig-build.css'))
+    .pipe(gulp.dest(config.sass.dest));
+
+});
+
