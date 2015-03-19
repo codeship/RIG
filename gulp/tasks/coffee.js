83eb121c6be732dbbf939b4bd7b534b60e147e7f
Roman Kuba <mail@codebryo.com>
2015-03-19 12:38:24 +0100

    Added Coffee Task

---
+++ gulp/tasks/coffee.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var plumber     = require('gulp-plumber');
+var cache       = require('gulp-cached');
+var include     = require('gulp-include');
+var coffee      = require('gulp-coffee');
+
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var config      = require('../config');
+
+gulp.task('coffee', function() {
+
+  return gulp.src(config.coffee.src)
+    .pipe(plumber())
+    .pipe(cache('coffee'))
+    .pipe(include())
+    .pipe(coffee(config.coffee.compiler))
+    .pipe(gulp.dest(config.coffee.dest))
+    .pipe(reload({stream: true}));
+
+});
+
