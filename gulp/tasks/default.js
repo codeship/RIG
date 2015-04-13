50896c85e6125faff0f1fe7f33b7b804ba602dfb
Codebryo <mail@codebryo.com>
2015-04-13 12:44:00 +0200

    Updated Gulp Task

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,7 +2,9 @@ var gulp        = require('gulp'),
     del         = require('del'),
     config      = require('../config');
 
-gulp.task('default',['clean','serve']);
+gulp.task('default',['clean'], function() {
+  gulp.start('serve');
+});
 
 gulp.task('clean', function() {
   del([config.paths.dev,config.paths.build,config.paths.deploy]);
